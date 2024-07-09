f38d6dcd5e192d7daf00a55534d48eafc3138db1
kongchairi <kongchairimarma8@gmail.com>
2024-07-09 19:35:00 +0600

    change

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyAplication extends StatelessWidget{
   Widget build(BuildContext context) {
    return const MaterialApp(
     debugShowCheckedModeBanner: false ,
-    home: Text("He"));
+    home: Text("Hello Kongchairai Marma"));
 
    
   }
