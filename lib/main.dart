786549a96f6b3df85fb51cf0cdec994c9e971e33
kongchairi <kongchairimarma8@gmail.com>
2024-07-09 19:25:42 +0600

    init

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+
+void main(){
+  runApp(MyAplication as Widget);
+}
+class MyAplication extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+   return const MaterialApp(
+    debugShowCheckedModeBanner: false ,
+    home: Text("He"));
+
+   
+  }
+  
+}
+
+
